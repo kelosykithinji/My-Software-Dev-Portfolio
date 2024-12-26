6da56b7f7fc10f12908fdefe6a587248369f7cae
kelosy kithinji <kelosykithinji@gmail.com>
2024-12-26 17:34:50 +0400

    Added a js plugin
    this plugin is responsible for the type script animation

---
+++ index.js
@@ -0,0 +1,10 @@
+//let heading = document.querySelector("h1");
+//heading.innerHTML = "Hello and welcome to my Portfolio, My name is Kelosy Muthoni and i am a front-end developer";
+
+new Typewriter("h1", {
+  strings:
+    "Hello and welcome to my Portfolio, My name is Kelosy Muthoni and i am a front-end developer",
+  autoStart: true,
+  
+});
+
